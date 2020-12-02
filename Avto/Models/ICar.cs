92d9c15af53296027f2385c25f70a4ed4dc3a2e8
Данил Цесаренко <danil-master@outlook.com>
2020-12-02 20:59:06 +0200

    Added interface Icar with method Brand, Classification, Engine, Color, Price

---
+++ Avto/Models/ICar.cs
@@ -0,0 +1,37 @@
+﻿namespace Avto.Models.Grocary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Common interface for any car.
+    /// </summary>
+    public interface ICar
+    {
+        /// <summary>
+        /// Gets brand of car.
+        /// </summary>
+        string Brand { get; }
+
+        /// <summary>
+        /// Gets Classification of car.
+        /// </summary>
+        string Classification { get; }
+
+        /// <summary>
+        /// Gets engine of car.
+        /// </summary>
+        string Engine { get; }
+
+        /// <summary>
+        /// Gets color of car.
+        /// </summary>
+        string Color { get; }
+
+        /// <summary>
+        /// Gets price of car.
+        /// </summary>
+        double Price { get; }
+    }
+}
