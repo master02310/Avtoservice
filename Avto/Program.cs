db97be6a7ca67ae204fe2ee75e62650c3184dece
Данил Цесаренко <danil-master@outlook.com>
2020-12-02 19:55:45 +0200

    Start programm

---
+++ Avto/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Avto
+{
+    using System;
+
+    /// <summary>
+    /// Start point of app.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Start of app.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
